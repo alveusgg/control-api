464f8946084b45d1a74b423edf1a17578871325a
merger3 <johndoe@example.com>
2026-01-02 01:02:13 -0500

    Minor language fix

--- src/utils.ts
+++ src/utils.ts
@@ -4,13 +4,13 @@ import * as constants from '@/constants';
 
 interface APIError {
 	code: number;
-	message: string;
+	details: string;
 }
 
 export function APIErrorResponse(ctx: Context<constants.Env>, status: number, code: number, error: Error): Response {
 	let newAPIError: APIError = {
 		code: code,
-		message: error.message
+		details: error.message
 	}
 	
 	ctx.status(status as StatusCode);
